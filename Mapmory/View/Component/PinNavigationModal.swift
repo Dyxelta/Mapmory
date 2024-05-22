c01443a938283be5e8b99a6ead04885faf908111
Dixon <dixonwillow23@gmail.com>
2024-05-22 16:13:45 +0700

    feat: create main page

--- Mapmory/View/Component/PinNavigationModal.swift
+++ Mapmory/View/Component/PinNavigationModal.swift
@@ -6,13 +6,39 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct PinNavigationModal: View {
+    var removePinAction: () -> Void
+    var navigateAction: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Get to know the history of where you pin")
+                .font(.headline)
+                .padding()
+            
+            Button(action: navigateAction) {
+                Text("Get from pinned location")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            .padding(.top)
+            
+            Button(action: removePinAction) {
+                Text("Remove Pin")
+                    .padding()
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            .padding()
+        }
+        .frame(maxWidth: 500)
+        .background(Color.white)
+        .cornerRadius(16)
+        .ignoresSafeArea(edges: .bottom)
     }
-}
-
-#Preview {
-    PinNavigationModal()
 }
--- Mapmory/View/Presentation/GetLocationPage.swift
+++ Mapmory/View/Presentation/GetLocationPage.swift
@@ -5,4 +5,80 @@
 //  Created by Dixon Willow on 21/05/24.
 //
 
-import Foundation
+import SwiftUI
+import MapKit
+import CoreLocation
+
+struct GetLocationPage: View {
+    
+    @StateObject private var locationManager = LocationManager()
+    
+    @State private var selectedCoordinate: CLLocationCoordinate2D?
+    
+    @State private var showHistoricalEventModal = false
+    @State private var showLocationErrorAlert = false
+    @State private var isAddPin = false
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            MapView(selectedCoordinate: $selectedCoordinate, isAddPin: $isAddPin)
+                .ignoresSafeArea(.all)
+            
+            Button(action: {
+                locationManager.getLocation()
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    if locationManager.cityName == "Unknown" {
+                        showLocationErrorAlert = true
+                    } else {
+                        showHistoricalEventModal = true
+                    }
+                }
+            }) {
+                Text("Get from current location")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            .padding()
+            .opacity(showHistoricalEventModal ? 0 : 1)
+            
+            if isAddPin, let coordinate = selectedCoordinate {
+                PinNavigationModal(
+                    removePinAction: {
+                        selectedCoordinate = nil
+                        isAddPin = false
+                    },
+                    navigateAction: {
+                        locationManager.reverseMapGeocode(coordinate: coordinate)
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                            if locationManager.cityName == "Unknown" {
+                                showLocationErrorAlert = true
+                            } else {
+                                showHistoricalEventModal = true
+                                isAddPin = false
+                            }
+                        }
+                    }
+                )
+                .transition(.move(edge: .bottom))
+                .ignoresSafeArea(edges: .bottom)
+            }
+        }
+        .sheet(isPresented: $showHistoricalEventModal, content: {
+            HistoricalEventModal(cityName: locationManager.cityName)
+        })
+        .alert(isPresented: $showLocationErrorAlert) {
+            Alert(
+                title: Text("Location error"),
+                message: Text("Cannot get the location properly"),
+                dismissButton: .default(Text("Dismiss"))
+            )
+        }
+    }
+}
+
+#Preview {
+    GetLocationPage()
+}
