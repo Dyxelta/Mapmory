60b61ae016445280856e35d10fea1daebcacf6cf
Dixon <dixonwillow23@gmail.com>
2024-05-22 16:13:07 +0700

    feat: create historical event page

--- Mapmory/View/Presentation/HistoricalEventModal.swift
+++ Mapmory/View/Presentation/HistoricalEventModal.swift
@@ -6,13 +6,75 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct HistoricalEventModal: View {
+    
+    var cityName: String
+    
+    @Query var locations: [Location]
+    
+    private var pickedLocation: Location {
+        locations.first { $0.cityName == cityName } ?? Location(cityName: "Unknown", cityDescription: "Unknown")
+    }
+    
+    @State private var expandedEvents: Set<String> = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                Text(pickedLocation.cityName)
+                    .font(.title)
+                    .padding()
+                
+                Text(pickedLocation.cityDescription)
+                    .padding()
+                
+                Divider()
+                    .padding()
+                
+                ForEach(pickedLocation.events) { eventData in
+                    VStack(alignment: .leading) {
+                        HStack (alignment: .center){
+                            Text("\(eventData.eventName) (\(eventData.eventYear)) - \(eventData.eventAspect)")
+                                .font(.headline)
+                            
+                            Spacer()
+                            
+                            Image(systemName: expandedEvents.contains(eventData.eventName) ? "chevron.up" : "chevron.down")
+                                .font(.body)
+                        }
+                        .padding(.vertical)
+                        .frame(maxWidth: UIScreen.main.bounds.width)
+                        .onTapGesture {
+                            withAnimation {
+                                toggleExpansion(eventData.eventName)
+                            }
+                        }
+                        
+                        Divider()
+                        
+                        if expandedEvents.contains(eventData.eventName) {
+                            Text(eventData.eventDesc)
+                                .padding(.vertical)
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .padding()
+        }
+    }
+    
+    private func toggleExpansion(_ eventName: String) {
+        if expandedEvents.contains(eventName) {
+            expandedEvents.remove(eventName)
+        } else {
+            expandedEvents.insert(eventName)
+        }
     }
 }
 
 #Preview {
-    HistoricalEventModal()
+    HistoricalEventModal(cityName: "Sample")
 }
